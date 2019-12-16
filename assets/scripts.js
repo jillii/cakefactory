eeb268cf92beca0b7f688804f59f1ea86faea927
jill <jho3292@gmail.com>
2019-12-16 00:22:36 -0500

    Update styles.

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,21 +11,27 @@ $('.page-link').click(function() {
 	$id = $this.attr("href");
 	$el_w_id = $($id);
 
-	scroll_to($el_w_id);
+	scroll_to($id);
 
 });
 
 $(document).ready(function(){
-	$target = window.location.hash;
+	$hash = window.location.hash;
 
-	if ($target) {
-		$section = $($target);
-		scroll_to($section);
+	if ($hash) {
+		scroll_to($hash);
+	} else {
+		$('#intro').addClass('active');
+		$('.page-link:first-of-type').addClass('active')
 	}
 });
 
-function scroll_to($section) {
+function scroll_to($id) {
+	$section = $($id);
+	$page_link = $(".page-link[href$='" + $id + "']");
 
 	$('section.active').removeClass('active');
-	$section.addClass('active');	
+	$('.page-link.active').removeClass('active');
+	$section.addClass('active');
+	$page_link.addClass('active');	
 }
